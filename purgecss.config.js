1e43b24f79bf7c36e24069470ea5e925ad32e4c1
jorissimaitis <js3700@bath.ac.uk>
2025-01-31 17:28:33 +0000

    a

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  content: ["_site/**/*.html", "_site/**/*.js"],
-  css: ["_site/assets/css/*.css"],
-  output: "_site/assets/css/",
-  skippedContentGlobs: ["_site/assets/**/*.html"],
+  content: [
+    "./_site/**/*.html",  // Path to your HTML files
+    "./index.html",       // Path to other HTML files
+    "./assets/js/**/*.js", // Path to JavaScript files if they contain dynamic classes
+  ],
+  css: [
+    "./assets/css/*.css",  // Path to your CSS files
+  ],
+  output: "./_site/assets/css/",  // Directory to save the purged CSS
 };
